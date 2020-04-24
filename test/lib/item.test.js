8bfd424bc7cb1e5f9d686a9e3906a92a3d33c16b
Joey Wilhelm <tarkatronic@gmail.com>
2020-04-24 14:54:03 -0400

    Adjust tests for proper getters

--- test/lib/item.test.js
+++ test/lib/item.test.js
@@ -1,13 +1,19 @@
 const assume = require("assume");
 const Item = require("../../lib/item");
 
-describe('Item', () => {
-  it('is not a container by default', () => {
+describe("Item", () => {
+  it("is not a container by default", () => {
     let item = new Item();
     assume(item._isContainer).is.false();
   });
-  it('throws an error on contentWeight by default', () => {
+
+  it("contentWeight is undefined by default", () => {
     let item = new Item();
-    assume(item.contentWeight).throws('is not a container');
+    assume(item.contentWeight).equals(undefined);
   });
+
+  it("cannot contain anything by default", () => {
+    let item = new Item();
+    assume(item.canContain()).is.false();
+  })
 });
