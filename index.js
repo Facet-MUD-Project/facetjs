a64bae6e234430d141c36c6cf810f30a0886bdca
Joey Wilhelm <tarkatronic@gmail.com>
2020-05-12 21:38:25 -0400

    Gracefully handle Ctrl+C

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const Server = require('./lib/server');
 
 const server = new Server();
 server.startServer();
+
+process.on('SIGINT', () => {
+  server.shutdown();
+  process.exit();  // eslint-disable-line no-process-exit
+});
