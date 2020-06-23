999b82a23ca3d4a0f58dfa7dc558025a0a648859
Joey Wilhelm <tarkatronic@gmail.com>
2020-06-22 21:27:27 -0400

    Make sure player input is pushed through their input handler

--- test/lib/game.test.ts
+++ test/lib/game.test.ts
@@ -3,6 +3,7 @@ import FakePlayer from './stubs';
 import Game from '../../src/lib/game';
 import { GameState } from '../../src/lib/base/enums';
 import sinon from 'sinon';
+import Player from '../../src/lib/base/player';
 
 describe('Game', () => {
   let game;
@@ -11,6 +12,11 @@ describe('Game', () => {
     // @ts-ignore
     Game.instance = null;
     game = Game.getInstance();
+  });
+
+  it('is a singleton', () => {
+    const game2 = Game.getInstance();
+    assume(game).equals(game2);
   });
 
   it('sends data to all players on broadcast', () => {
@@ -55,6 +61,27 @@ describe('Game', () => {
     game.gameLoop();
     game.players.forEach((player) => {
       assume(player.flushOutput.calledOnce).is.true();
+    });
+  });
+
+  it("pushes buffered input through the player's input handler during game loop", () => {
+    let handler = null, handlers = [];
+    game._players = [new Player(), new Player(), new Player()];
+    game.players.forEach(
+      (player) => {
+        sinon.stub(player, 'remoteAddress').get(() => '127.0.0.1');
+        sinon.stub(player, 'inputHandler').get(() => {
+          handler = sinon.stub();
+          handlers.push(handler);
+          return {handleInput: handler};
+        });
+        player.receiveData('foo');
+      }
+    );
+    game.shutdown();
+    game.gameLoop();
+    handlers.forEach((handler) => {
+      assume(handler.called).is.true();
     });
   });
 });
