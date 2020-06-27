0bca68b188d3d87762f03605928f864bb2e004d3
Joey Wilhelm <tarkatronic@gmail.com>
2020-06-27 09:27:11 -0400

    Add 100% test coverage for containers

--- test/lib/base/container.test.ts
+++ test/lib/base/container.test.ts
@@ -1,5 +1,7 @@
 import assume from 'assume';
 import Container from '../../../src/lib/base/container';
+import Room from '../../../src/lib/base/room';
+import { CannotContain, AlreadyContains, CannotRelease } from '../../../src/lib/base/exceptions';
 
 describe('Container', () => {
   let container: Container;
@@ -37,5 +39,52 @@ describe('Container', () => {
     container.addObject(other1);
     container.addObject(other2);
     assume(container.contentWeight).equals(42);
+  });
+
+  it('cannot contain more than max number of items', () => {
+    const other1 = new Container(), other2 = new Container();
+    container.maxContentCount = 1;
+    container.addObject(other1);
+    assume(() => container.addObject(other2)).throws(CannotContain);
+  });
+
+  it('cannot contain more than its max weight', () => {
+    const other = new Container();
+    container.maxContentWeight = 1.0;
+    other.weight = 42;
+    assume(() => container.addObject(other)).throws(CannotContain);
+  });
+
+  it('cannot add an object which it already contains', () => {
+    const other = new Container();
+    container.addObject(other);
+    assume(() => container.addObject(other)).throws(AlreadyContains);
+  });
+
+  it('cannot contain an object which does not allow itself to be contained', () => {
+    const room = new Room();
+    assume(() => container.addObject(room)).throws(CannotContain);
+  });
+
+  it('cannot release objects which it does not contain', () => {
+    const other = new Container();
+    assume(container.canRelease(other)).is.false();
+  });
+
+  it('can release objects which it does contain', () => {
+    const other = new Container();
+    container.addObject(other);
+    assume(container.canRelease(other)).is.true();
+  });
+
+  it('will let items be removed from it', () => {
+    const other = new Container();
+    container.addObject(other).removeObject(other);
+    assume(container.contents).to.be.empty();
+  });
+
+  it('errors when removing an object which it cannot release', () => {
+    const other = new Container();
+    assume(() => container.removeObject(other)).to.throw(CannotRelease);
   });
 });
