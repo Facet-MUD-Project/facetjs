4921dcca50be21f068db39ec3817fa1880bcfe23
Thomas L. Martin <daftpun@yahoo.com>
2020-07-13 08:28:52 -0700

    Fixing more formatting errors, vscode apparently lost it's damn mind and thought it was formatting for... 'rust'????

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -7,49 +7,49 @@ import Player from './base/player';
  * A class representing the game loop and player queue
  */
 export default class Game {
- public  motd : string = null;
- private  static instance : Game;
- private  _players : Array<Player> = [];
- private  _state : GameState = GameState.STARTING;
+ public  motd: string = null;
+ private  static instance: Game;
+ private  _players: Array<Player> = [];
+ private  _state: GameState = GameState.STARTING;
 
  private
   constructor() {}  // eslint-disable-line
 
-  static getInstance() : Game {
+  static getInstance(): Game {
     if (!Game.instance) Game.instance = new Game();
     return Game.instance;
   }
 
-  addPlayer(player : Player) : Game {
+  addPlayer(player : Player): Game {
     this._players.push(player);
     return this;
   }
 
-  get players() : Array<Player> { return this._players; }
+  get players(): Array<Player> { return this._players; }
 
-  async gameLoop() : Promise<void> {
-    this.players.forEach((player) = > {
+  async gameLoop(): Promise<void> {
+    this.players.forEach((player) => {
       player.inputBuffer.forEach(
-          (msg) = > player.inputHandler.handleInput(player, msg));
+          (msg) => player.inputHandler.handleInput(player, msg));
       player.flushOutput();
     });
-    if (this._state == = GameState.RUNNING)
-      setTimeout(() = > this.gameLoop());
-    else if (this._state == = GameState.SHUTTING_DOWN)
+    if (this._state === GameState.RUNNING)
+      setTimeout(() => this.gameLoop());
+    else if (this._state === GameState.SHUTTING_DOWN)
       this._state = GameState.SHUTDOWN;
   }
 
-  async broadcast(msg : string) : Promise<void> {
+  async broadcast(msg: string): Promise<void> {
     console.debug('[debug] Broadcasting message: ' + msg.trim());
-    await Promise.all(this.players.map(async(player) = > player.sendData(msg)));
+    await Promise.all(this.players.map(async(player) => player.sendData(msg)));
   }
 
   /**
    * Read the MOTD file from disk and store it in a local property
    */
-  readMOTD() : void {
+  readMOTD(): void {
     fs.readFile(
-        path.join(__dirname, '..', 'MOTD'), (err, data) = > {
+        path.join(__dirname, '..', 'MOTD'), (err, data) => {
           if (err) {
             console.error(err);
             return;
@@ -58,13 +58,13 @@ export default class Game {
         });
   }
 
-  async startUp() : Promise<void> {
+  async startUp(): Promise<void> {
     this.readMOTD();
     this._state = GameState.RUNNING;
     this.gameLoop();
   }
 
-  shutdown() : void { this._state = GameState.SHUTTING_DOWN; }
+  shutdown(): void { this._state = GameState.SHUTTING_DOWN; }
 
-  get state() : GameState { return this._state; }
+  get state(): GameState { return this._state; }
 }
--- src/lib/server.ts
+++ src/lib/server.ts
@@ -8,10 +8,10 @@ import Player from './base/player';
  * The socket server used to accept user connections and handle data I/O
  */
 export default class Server {
- public  address : string;
- public  port : number;
- private  _server : net.Server = null;
- private  _game : Game = null;
+ public  address: string;
+ public  port: number;
+ private  _server: net.Server = null;
+ private  _game: Game = null;
 
   constructor({address = '::', port = 8000} = {}) {
     this.address = address;
@@ -21,8 +21,8 @@ export default class Server {
   /**
    * Create and start the TCP server on the configured address/port
    */
-  async startServer() : Promise<void> {
-    this._server = net.createServer((conn) = > this.onConnect(conn));
+  async startServer(): Promise<void> {
+    this._server = net.createServer((conn) => this.onConnect(conn));
     this._server.listen(this.port, this.address);
     this._game = Game.getInstance();
     await this._game.startUp();
@@ -32,12 +32,11 @@ export default class Server {
   /**
    * Handle a new connection to the server
    */
-  async onConnect(conn : net.Socket) : Promise<void> {
+  async onConnect(conn: net.Socket): Promise<void> {
     console.debug('[debug] Incoming connection from ' + conn.remoteAddress);
     const telnetSocket = new TelnetSocket(conn);
     const player = new Player(telnetSocket);
-    telnetSocket.on('data', (data
-                             : string) = > player.receiveData(data.toString()));
+    telnetSocket.on('data', (data: string) => player.receiveData(data.toString()));
     await player.sendData(this._game.motd);
     await player.sendData('What... is your name? ');
     await this._game.broadcast('A new player has entered the game!\r\n');
@@ -47,13 +46,13 @@ export default class Server {
   /**
    * Shut down the server, first informing and disconnecting all players
    */
-  async shutdown() : Promise<void> {
+  async shutdown(): Promise<void> {
     console.info('[info] Shutting down server...');
     this._game.broadcast('The server is being shut down! Goodbye.\r\n');
     this._game.shutdown();
     this._game.gameLoop();
     await Promise.all(
-        this._game.players.map(async(player) = > player.disconnect()));
+        this._game.players.map(async(player) => player.disconnect()));
     this._server.close();
   }
 }
