f6b05c9e9178c11e182bce8b3a185fd4320980a0
Thomas L. Martin <daftpun@yahoo.com>
2020-07-13 08:19:21 -0700

    Moved MOTD related functionality into game.

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -1,58 +1,70 @@
-import { GameState } from './base/enums';
+import* as fs from 'fs';
+import* as path from 'path';
+import{GameState} from './base/enums';
 import Player from './base/player';
 
 /**
  * A class representing the game loop and player queue
  */
 export default class Game {
-  private static instance: Game;
-  private _players: Array<Player> = [];
-  private _state: GameState = GameState.STARTING;
+ public  motd : string = null;
+ private  static instance : Game;
+ private  _players : Array<Player> = [];
+ private  _state : GameState = GameState.STARTING;
 
-  private constructor() { }  // eslint-disable-line
+ private
+  constructor() {}  // eslint-disable-line
 
-  static getInstance(): Game {
+  static getInstance() : Game {
     if (!Game.instance) Game.instance = new Game();
     return Game.instance;
   }
 
-  addPlayer(player: Player): Game {
+  addPlayer(player : Player) : Game {
     this._players.push(player);
     return this;
   }
 
-  get players(): Array<Player> {
-    return this._players;
-  }
+  get players() : Array<Player> { return this._players; }
 
-  async gameLoop(): Promise<void> {
-    this.players.forEach((player) => {
+  async gameLoop() : Promise<void> {
+    this.players.forEach((player) = > {
       player.inputBuffer.forEach(
-        (msg) => player.inputHandler.handleInput(player, msg)
-      );
+          (msg) = > player.inputHandler.handleInput(player, msg));
       player.flushOutput();
     });
-    if (this._state === GameState.RUNNING) setTimeout(() => this.gameLoop());
-    else if (this._state === GameState.SHUTTING_DOWN) this._state = GameState.SHUTDOWN;
+    if (this._state == = GameState.RUNNING)
+      setTimeout(() = > this.gameLoop());
+    else if (this._state == = GameState.SHUTTING_DOWN)
+      this._state = GameState.SHUTDOWN;
   }
 
-  async broadcast(msg: string): Promise<void> {
+  async broadcast(msg : string) : Promise<void> {
     console.debug('[debug] Broadcasting message: ' + msg.trim());
-    await Promise.all(
-      this.players.map(async (player) => player.sendData(msg))
-    );
+    await Promise.all(this.players.map(async(player) = > player.sendData(msg)));
   }
 
-  async startUp(): Promise<void> {
+  /**
+   * Read the MOTD file from disk and store it in a local property
+   */
+  readMOTD() : void {
+    fs.readFile(
+        path.join(__dirname, '..', 'MOTD'), (err, data) = > {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          this.motd = data.toString();
+        });
+  }
+
+  async startUp() : Promise<void> {
+    this.readMOTD();
     this._state = GameState.RUNNING;
     this.gameLoop();
   }
 
-  shutdown(): void {
-    this._state = GameState.SHUTTING_DOWN;
-  }
+  shutdown() : void { this._state = GameState.SHUTTING_DOWN; }
 
-  get state(): GameState {
-    return this._state;
-  }
+  get state() : GameState { return this._state; }
 }
